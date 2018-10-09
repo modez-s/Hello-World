e83ea68434e3e111f2bf300df6a7ec43b1530d54
modez-s <sebastien.maudet@univ-nantes.fr>
2018-10-09 13:42:54 +0200

    Ajout des sources du projet

---
+++ src/main_HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * @author Sebastien Maudet - IUT RT La Roche sur Yon
+ * Cette application a été développé dans un environnement JAVA 8 sous eclipse
+ * Application permettant d'afficher "Hello World"
+*/
+
+import javax.swing.JOptionPane;
+
+public class main_HelloWorld {
+
+	// Méthode principale de l'application
+	public static void main(String[] args) {
+		// Afficher dans la console, le texte "Hello la LP DIP"
+		System.out.println("Hello la LP DIP !!!");
+		
+		// Afficher dans une fenêtre le texte "En direct de LR"
+		JOptionPane.showMessageDialog(null, "En direct de LR");
+	}
+}
