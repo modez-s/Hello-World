3264fa8bacbfb754257e788d6ab9a76d832ee46f
modez-s <sebastien.maudet@univ-nantes.fr>
2018-10-09 17:32:52 +0200

    Version du projet avec modifications

--- src/main_HelloWorld.java
+++ src/main_HelloWorld.java
@@ -12,8 +12,8 @@ public class main_HelloWorld {
 	public static void main(String[] args) {
 		// Afficher dans la console, le texte "Hello la LP DIP"
 		System.out.println("Hello la LP DIP !!!");
-		
+
 		// Afficher dans une fenêtre le texte "En direct de LR"
-		JOptionPane.showMessageDialog(null, "En direct de LR");
+		JOptionPane.showMessageDialog(null, "En direct de La Roche sur Yon");
 	}
 }
